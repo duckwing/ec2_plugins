ef45e03167ece75c146d3eece43e39eea31b0ea3
John Doe <jd@hp>
2015-08-02 16:09:51 +0300

    added small boto config

--- bootstrap.py
+++ bootstrap.py
@@ -7,10 +7,16 @@ def main():
     if len(sys.argv) != 2:
         usage()
         return 1
+    conf = get_config()
+    print(conf['plugins'])
     plugin, func = sys.argv[1].split('.', 1)
     print('Running {}.{}'.format(plugin, func))
     hook_plugins()
     return getattr(import_plugin(plugin), func)()
+
+def get_config():
+    from boto.pyami.config import Config
+    return Config()
 
 def import_plugin(plugin_name):
     return importlib.import_module('plugins.' + plugin_name)
